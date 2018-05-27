c559eba17158676e52a4e58d30c975277a3acd19
Silvan Knecht <knecht.silvan@gmail.com>
2018-05-27 20:51:17 +0200

    new vocabulary

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -10,70 +10,71 @@ var lesson12 = {
     "achievement": "Errungenschaft",
     "appraisal": "Bewertung",
     "assertivness": "Durchsetzungsvermögen",
-	"assessment": "Bewertung",
-	"cardboard": "Karton",
-	"co-operation": "Zusammenarbeit",
-	"contingency plan": "Notfallplan",
-	"criteria": "Kriterien",
-	"criterion":"Kriterium",
-	"drop": "fallen",
-	"effort": "Anstrengung",
-	"electricity grid": "Stromnetz",
-	"evaluation": "Auswertung",
-	"gravity": "Erdanziehungskraft",
-	"greenhouse": "Gewächshaus",
-	"nutrient": "Nährstoff",
-	"objective": "Zielsetzung",
-	"participation": "Beteiligung",
-	"performance": "Performance",
-	"schedule": "Zeitplan",
-	"scope": "Umfang",
-	"seawater": "Meerwasser",
-	"tender": "zärtlich",
-	"water vapour": "Wasserdampf",
-	"graphene sheet": "Graphenblatt",
-	"graphite": "Graphit",
-	"nanofluid": "Nanofluid",
-	"nanogenerator": "Nanogenerator",
-	"nanometre": "Nanometer",
-	"nanoparticle": "Nanopartikel",
-	"nanoscale": "nanoskalig",
-	"nanoscale pore": "nanoskalige Pore",
-	"nanowire": "Nanodraht",
-	"smart dust": "intelligenter Staub",
-	"transparency": "Transparenz",
-	"transparent": "transparent",
-	"assess": "beurteilen",
-	"attempt(noun)": "Versuch",
-	"copy": "kopieren",
-	"expand on": "erweitern",
-	"humidity": "Feuchtigkeit",
-	"modify": "ändern",
-	"reckon": "rechnen",
-	"rely on": "sich verlassen auf",
-	"roll out": "ausrollen",
-	"saturate": "sättigen",
-	"trickle": "Rinnsal",
-	"come up": "ausdenken",
-	"get hold of": "ergattern",
-	"get on": "weiter kommen",
-	"go about": "in Angriff nehmen",
-	"go ahead": "mach weiter",
-	"hang on": "abwarten",
-	"turn out": "herauskommen",
-	"go bankrupt": "Bankrott gehen",
-	"essential": "wesentlich",
-	"super-saturated": "übersättigt"
+    "assessment": "Bewertung",
+    "cardboard": "Karton",
+    "co-operation": "Zusammenarbeit",
+    "contingency plan": "Notfallplan",
+    "criteria": "Kriterien",
+    "criterion": "Kriterium",
+    "drop": "fallen",
+    "effort": "Anstrengung",
+    "electricity grid": "Stromnetz",
+    "evaluation": "Auswertung",
+    "gravity": "Erdanziehungskraft",
+    "greenhouse": "Gewächshaus",
+    "nutrient": "Nährstoff",
+    "objective": "Zielsetzung",
+    "participation": "Beteiligung",
+    "performance": "Performance",
+    "schedule": "Zeitplan",
+    "scope": "Umfang",
+    "seawater": "Meerwasser",
+    "tender": "zärtlich",
+    "water vapour": "Wasserdampf",
+    "graphene sheet": "Graphenblatt",
+    "graphite": "Graphit",
+    "nanofluid": "Nanofluid",
+    "nanogenerator": "Nanogenerator",
+    "nanometre": "Nanometer",
+    "nanoparticle": "Nanopartikel",
+    "nanoscale": "nanoskalig",
+    "nanoscale pore": "nanoskalige Pore",
+    "nanowire": "Nanodraht",
+    "smart dust": "intelligenter Staub",
+    "transparency": "Transparenz",
+    "transparent": "transparent",
+    "assess": "beurteilen",
+    "attempt(noun)": "Versuch",
+    "copy": "kopieren",
+    "expand on": "erweitern",
+    "humidity": "Feuchtigkeit",
+    "modify": "ändern",
+    "reckon": "rechnen",
+    "rely on": "sich verlassen auf",
+    "roll out": "ausrollen",
+    "saturate": "sättigen",
+    "trickle": "Rinnsal",
+    "come up": "ausdenken",
+    "get hold of": "ergattern",
+    "get on": "weiter kommen",
+    "go about": "in Angriff nehmen",
+    "go ahead": "mach weiter",
+    "hang on": "abwarten",
+    "turn out": "herauskommen",
+    "go bankrupt": "Bankrott gehen",
+    "essential": "wesentlich",
+    "super-saturated": "übersättigt"
 
 };
-var questions = lesson11;
+var questions;
 
 
-var currentQuestion = 0;
-var maxQuestion = Object.keys(questions).length - 1;
+var currentQuestion;
+var maxQuestion;
+var maxQuestionTot;
 
-var question = Object.keys(questions)[currentQuestion];
-var answer = questions[question];
+var question;
+var answer;
 
 $(document).ready(function () {
 
@@ -83,13 +84,30 @@ $(document).ready(function () {
     $("#checkanswer").click(function () {
         let userAnswer = $("#answer").val();
 
+        //Correct Answer
         if (userAnswer === answer) {
+            if ($("#answer").hasClass("wrongAnswer")) {
+
+            } else {
+                delete questions[question];
+            }
             $("#answer").removeClass("wrongAnswer").addClass("rightAnswer");
             $("#showanswer").css("display", "none");
+
             setTimeout(nextQuestion, 200);
+
+            //Wrong Answer
         } else {
             $("#answer").removeClass("rightAnswer").addClass("wrongAnswer");
             $("#showanswer").css("display", "inline");
+            $("#showanswer").keypress(function (e) {
+                if (e.which == 13) {
+                    alert('You pressed enter!');
+                }
+            });
+
+            delete questions[question];
+            questions[question] = answer;
 
         }
 
@@ -99,19 +117,22 @@ $(document).ready(function () {
     $("#showanswer").click(function () {
 
         $("#answer").val(answer);
+        $('#checkanswer').prop('disabled', false);
+
 
     });
 
 
     $("#lesson11").click(function () {
 
-        questions = lesson11;
+
+        questions = JSON.parse(JSON.stringify(lesson11));
         currentQuestion = 0;
-        maxQuestion = Object.keys(questions).length - 1;
+        maxQuestionTot = Object.keys(questions).length - 1;
 
-         question = Object.keys(questions)[currentQuestion];
-         answer = questions[question];
-
+        question = Object.keys(questions)[currentQuestion];
+        answer = questions[question];
+        $("#progress-bar").css("width", "0%");
         $("#question").val(question);
 
 
@@ -120,13 +141,13 @@ $(document).ready(function () {
 
     $("#lesson12").click(function () {
 
-        questions = lesson12;
+        questions = JSON.parse(JSON.stringify(lesson12));
         currentQuestion = 0;
-        maxQuestion = Object.keys(questions).length - 1;
+        maxQuestionTot = Object.keys(questions).length - 1;
 
         question = Object.keys(questions)[currentQuestion];
         answer = questions[question];
-
+        $("#progress-bar").css("width", "0%");
         $("#question").val(question);
 
     });
@@ -135,14 +156,16 @@ $(document).ready(function () {
 });
 
 function nextQuestion() {
+
     $("#answer").removeClass("wrongAnswer rightAnswer");
     $("#answer").val("");
+    $('#checkanswer').prop('disabled', true);
+    maxQuestion = Object.keys(questions).length - 1;
+    var progress = (maxQuestionTot - maxQuestion) * 100 / (maxQuestionTot + 1);
+    $("#progress-bar").css("width", progress + "%");
 
-    if (currentQuestion < maxQuestion) {
-        currentQuestion++;
-    } else {
-        currentQuestion = 0;
-    }
+
+    currentQuestion = Math.floor((Math.random() * maxQuestion));
 
     question = Object.keys(questions)[currentQuestion];
     answer = questions[question];
@@ -153,9 +176,9 @@ function nextQuestion() {
 }
 
 
-$(document).ready(function(){
-    $('#checkanswer').prop('disabled',true);
-    $('#answer').keyup(function(){
+$(document).ready(function () {
+    $('#checkanswer').prop('disabled', true);
+    $('#answer').keyup(function () {
         $('#checkanswer').prop('disabled', this.value == "" ? true : false);
     })
 });
